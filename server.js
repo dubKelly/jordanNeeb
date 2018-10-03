e6b4ece64d1263f1e3bf2bd590fdfceb6aba0c49
dubKelly <js.neeb1780@gmail.com>
2018-10-02 23:25:14 -0400

    first deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const email = require('./routes/email');
 
@@ -28,6 +29,15 @@ app.use(function(req, res, next) {
 
 // Routes
 app.use('/email', email);
+
+// Serve static in production
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 
